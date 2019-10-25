96a5536f30ba3b65c1db2c93ba6a22081dbd3d3f
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-10-25 12:13:01 -0700

    adds jscript to call on quiz button to open modal, connects exit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
+
+  var modal = document.getElementById("targetModal");
+  var exit = document.getElementsByClassName("close")[0];
+  var quizBtn = document.getElementById("quizBtn");
+
+  quizBtn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  exit.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
   $("form#questions").submit(function(event) {
-    
+
+
+
     event.preventDefault();
   });
 });
