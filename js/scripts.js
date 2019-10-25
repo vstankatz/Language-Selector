e76de48299f5d4b7d65b2024b117e025b5b2c14f
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-10-25 12:55:26 -0700

    adds variable definitions to js.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var quizBtn = document.getElementById("quizBtn");
 
   quizBtn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = "inline-block";
   }
 
   exit.onclick = function() {
@@ -19,6 +19,22 @@ $(document).ready(function() {
   }
 
   $("form#questions").submit(function(event) {
+      var questionOneA = $("input#questionOneA");
+      var questionOneB = $("input#questionOneB");
+      var questionOneC = $("input#questionOneC");
+      var questionTwoA = $("input#questionTwoA");
+      var questionTwoB = $("input#questionTwoB");
+      var questionTwoC = $("input#questionTwoC");
+      var questionThreeA = $("input#questionThreeA");
+      var questionThreeB = $("input#questionThreeB");
+      var questionThreeC = $("input#questionThreeC");
+      var questionFourA = $("input#questionFourA");
+      var questionFourB = $("input#questionFourB");
+      var questionFourC = $("input#questionFourC");
+      var questionFiveA = $("input#questionFiveA");
+      var questionFiveB = $("input#questionFiveB");
+      var questionFiveC = $("input#questionFiveC");
+
 
 
 
